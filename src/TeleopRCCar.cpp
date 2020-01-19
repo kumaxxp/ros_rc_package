91250ef5260d1a9296431a919a91b34f763e7a2a
kumaxxp <kuma_diy@hotmail.com>
2020-01-19 11:55:15 +0900

    ファイル名を適正に修正

---
+++ src/TeleopRCCar.cpp
@@ -0,0 +1,42 @@
+// TeleopRCCar.cpp
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+
+class TeleopRCCar
+{
+    public:
+    TeleopRCCar();
+
+    private:
+    void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+
+    ros::NodeHandle m_nh;
+    ros::Publisher  m_pub;
+    ros::Subscriber m_sub;
+};
+
+TeleopRCCar::TeleopRCCar()
+{
+    m_pub = m_nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+    m_sub = m_nh.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopRCCar::joyCallback, this);
+}
+
+void TeleopRCCar::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+{
+    double dScale = 1;
+
+    geometry_msgs::Twist twist;
+    twist.angular.x = dScale*joy->axes[0];
+    twist.linear.x = dScale*joy->axes[1];
+    m_pub.publish(twist);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "teleop_rccar");
+    TeleopRCCar teleop_rccar;
+
+    ros::spin();
+}
