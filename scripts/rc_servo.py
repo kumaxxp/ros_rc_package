3e3efb687c18de105e95dd8fe2c3290415150e22
kumaxxp <kuma_diy@hotmail.com>
2020-02-01 13:32:31 +0900

    2軸動くように変更

--- scripts/rc_servo.py
+++ scripts/rc_servo.py
@@ -20,15 +20,20 @@ class rc_servo():
         # アナログ左スティック（左側＋、右側ー）
         a =  90*joy_msg.axes[0]/1.3
         pos = 90 +int(a)
-
         pulse = (650-150)*pos/180+150+(-10)
+        duty = int( float(pos) * 2.17 + 102 )
+        self.pwm.set_pwm(0, 0, duty)
+
+        a =  90*joy_msg.axes[1]/1.3
+        pos = 90 +int(a)
+        pulse = (650-150)*pos/180+150+(-10)
+        duty = int( float(pos) * 2.17 + 102 )
+        self.pwm.set_pwm(1, 0, duty)
         #self.pwm.set_pwm(0, 0, pulse)
 
         rospy.loginfo(joy_msg)
 
-        duty = int( float(pos) * 2.17 + 102 )
-        self.pwm.set_pwm(0, 0, duty)
-        print "a,duty,button0" , a, duty, joy_msg.buttons[0]
+
 
 if __name__ == '__main__':
     # nodeの立ち上げ
