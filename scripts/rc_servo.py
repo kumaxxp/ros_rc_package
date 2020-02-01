e8d13e566353813c9d3cfb3449028de3835b413f
kumaxxp <kuma_diy@hotmail.com>
2020-02-01 11:33:28 +0900

    axesではなくbottonを取ろうとしていた

--- scripts/rc_servo.py
+++ scripts/rc_servo.py
@@ -18,16 +18,17 @@ class rc_servo():
 
     def callback(self, joy_msg):
         # アナログ左スティック（左側＋、右側ー）
-        a =  90*joy_msg.buttons[0]/1.3
+        a =  90*joy_msg.axes[0]/1.3
         pos = 90 +int(a)
 
         pulse = (650-150)*pos/180+150+(-10)
-        self.pwm.set_pwm(0, 0, pulse)
+        #self.pwm.set_pwm(0, 0, pulse)
 
         rospy.loginfo(joy_msg)
 
-        #duty = int( float(angle) * 2.17 + 102 )
-        #self.pwm.set_pwm(0, 0, duty)
+        duty = int( float(pos) * 2.17 + 102 )
+        self.pwm.set_pwm(0, 0, duty)
+        print "a,duty,button0" , a, duty, joy_msg.buttons[0]
 
 if __name__ == '__main__':
     # nodeの立ち上げ
